14678fc6b1ae54d67e3f989f29285a9f4707a121
PayneQin <qinyuanpei@chinasofti.com>
2018-02-24 18:31:32 +0800

    Update Blog

---
+++ index.js
@@ -0,0 +1,33 @@
+var front = require('../hexo-front-matter');
+var fs = require('../hexo-fs');
+
+/*
+ * Hexo过滤器扩展定义
+ */
+hexo.extend.filter.register('before_post_render',function(data){
+    issueId = loadIssueId(data);
+    if(issueId != 'undefined') return data
+    data = initIssues(data)
+    console.log(data.issueId)
+    return data
+});
+
+/*
+ * 从Markdown文件中加载issueId
+ * @param data: 元数据
+ */
+function loadIssueId(data) {
+    var match = data.content.match(/issueId:.*\n/);
+    console.log(match)
+    if (match) return match[0].split(':')[1].trim();
+    return 'undefined';
+}
+
+/*
+ * 通过Github API为博客初始化Issues
+ * @param data：元数据
+ */
+var logic = require('./lib/logic')
+function initIssues(data){
+    return logic(data)
+}
